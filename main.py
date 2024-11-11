e9d549cb912c9c45fe0cee85a6ba9fd4bd865f49
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-11-11 17:03:37 +0100

    generate sql in out dir

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 # importing required classes
 from pypdf import PdfReader
 from pathlib import Path
-import re, locale
+import re
 import matplotlib.pyplot as plt
-
-def extract_sections_regex(text, start_pattern: str, end_pattern: str):
-    pattern = f'{start_pattern}(.*?){end_pattern}'
-    matches = re.findall(pattern, text, re.DOTALL)
-    # Split matches into lines and remove empty lines
-    return [match.strip().split('\n') for match in matches]
 
 def extract_sections(text, start_pattern: str, end_pattern: str):
     section = []
@@ -28,24 +22,37 @@ def analyse_livret(text):
         match = re.search(r' (\d{7,}) ', line) # au moins 7 chiffres
         if match:
             numero_compte = match[1]
-            print(f'Nouveau compte trouvé: {numero_compte}')
+            # print(f'Nouveau compte trouvé: {numero_compte}')
         if ' SOLDE' in line and 'Réf' in line:
             match = re.search(r'AU (\d.+\d) (\d{1,3}(\.?\d{3})*,\d+)', line)
             date_solde = match[1]
             solde = match[2]
             solde = solde.replace('.','').replace(',','.')
             solde = float(solde)
-            print(f'{numero_compte},{date_solde},{solde}')
+            # print(f'{numero_compte},{date_solde},{solde}')
             return [date_solde, numero_compte, solde]    
+
+def analyse_autres_comptes(date_solde, text):
+    for line in text:
+        print(line)
+        if 'LIVRET DE DEVELOPPEMENT DURABLE SOLIDAIRE' in line:
+            match = re.search(r'(\d.+\d) .+EUR .(\d{1,3}(\.?\d{3})*,\d+)', line)
+            if match:
+                numero_compte = match[1]
+                solde = match[2]
+                solde = solde.replace('.','').replace(',','.').replace('+','')
+                solde = float(solde)
+                return [date_solde, numero_compte, solde]    
+
+############## 
 
 def main():
     
     # print('date,solde')
     tab_date = []
     tab_solde = []
-    titulaire = None
-    compte = None
-
+    lines_found = 0
+    result_file = open("out\insert.sql", "w")
     dir_path = Path('.\\pdf')
     for file in sorted(dir_path.glob('*.pdf')):
         print(f'Processing {file} ')
@@ -61,17 +68,27 @@ def main():
             if solde:
                 tab_date.append(solde[0])
                 tab_solde.append(solde[2])
+                # result_file.writelines(f"INSERT INTO epargne (date, compte, solde) VALUES (TO_DATE('{solde[0]}','DD/MM/YYYY'), '{solde[1]}', '{solde[2]}');\n")
+                lines_found += 1
+                solde_ldd = analyse_autres_comptes(solde[0], ldd)
+                if solde_ldd:
+                    tab_date.append(solde_ldd[0])
+                    tab_solde.append(solde_ldd[2])
+                    result_file.writelines(f"INSERT INTO epargne (date, compte, solde) VALUES (TO_DATE('{solde_ldd[0]}','DD/MM/YYYY'), '{solde_ldd[1]}', '{solde_ldd[2]}');\n")
+                    lines_found += 1
 
-        #     if 'SITUATION DE VOS AUTRES COMPTES' in line:
-        #         processing_autres_comptes = True
-            # Plotting
-    plt.plot(tab_date,tab_solde)
-    plt.xlabel('Date')
-    plt.ylabel('Solde')
-    plt.title(f'Epargne')
 
-    # function to show the plot
-    plt.show()
+    result_file.close()
+
+    print(f'{lines_found} lines found.')
+    # # Plotting
+    # plt.plot(tab_date,tab_solde)
+    # plt.xlabel('Date')
+    # plt.ylabel('Solde')
+    # plt.title(f'Epargne')
+
+    # # function to show the plot
+    # plt.show()
 
 
 if __name__ == "__main__":
