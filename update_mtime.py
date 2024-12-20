24beb172c2d01add3cd5e1223e6b6bdd9ef0872a
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-12-20 16:41:12 +0100

    add . log and count in updatemtime

--- update_mtime.py
+++ update_mtime.py
@@ -2,9 +2,13 @@ import os, datetime, time
 from pathlib import Path
 
 dir_path = Path('.\\pdf')
+count = 0
 for file in sorted(dir_path.glob('*.pdf')):
-    print(file)
+    print('.', end='')
     date_string = file.name.rsplit(' ', 1)[1].replace('.pdf','')
     dt = datetime.datetime.strptime(date_string,'%Y-%m-%d')
     unix_time = time.mktime(dt.timetuple())
     os.utime(file, (unix_time, unix_time))
+    count += 1
+
+print(f'\nFiles processed: {count}')
