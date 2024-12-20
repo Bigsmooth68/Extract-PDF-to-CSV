e86b1224e1e4593da360ff7010782b862f05e5e4
Bigsmooth68 <olivier.spiesser@gmail.com>
2024-12-20 16:30:17 +0100

    add log to update mtime

--- update_mtime.py
+++ update_mtime.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 dir_path = Path('.\\pdf')
 for file in sorted(dir_path.glob('*.pdf')):
-    file_name = file.name
-    date_string = file_name.rsplit(' ', 1)[1].replace('.pdf','')
-    print(date_string)
+    print(file)
+    date_string = file.name.rsplit(' ', 1)[1].replace('.pdf','')
     dt = datetime.datetime.strptime(date_string,'%Y-%m-%d')
     unix_time = time.mktime(dt.timetuple())
     os.utime(file, (unix_time, unix_time))
