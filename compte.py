b077bf9c9bb43981af5efe4eae2bc76de51518de
Bigsmooth68 <olivier.spiesser@gmail.com>
2025-01-02 09:46:18 +0100

    sql: ajout d'une ligne après truncate

--- compte.py
+++ compte.py
@@ -33,7 +33,7 @@ class compte:
         if len(self.lignes) > 0:
             self.lignes.drop_duplicates(inplace=True)
             with open(f"out/{table}.sql", "w", encoding="utf-8") as f:
-                f.writelines(f"TRUNCATE TABLE {table};\n")
+                f.writelines(f"TRUNCATE TABLE {table};\n###\n")
                 for index, row in self.lignes.iterrows():
                     date_solde = row["date"].strftime("%Y-%m-%d")
                     numero_compte = row["compte"]
