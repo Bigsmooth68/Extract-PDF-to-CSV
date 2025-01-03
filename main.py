e006855646ed94979114bd9bf9b02a578f008c14
Bigsmooth68 <olivier.spiesser@gmail.com>
2025-01-03 09:15:27 +0100

    renommage variable

--- main.py
+++ main.py
@@ -98,11 +98,11 @@ def main():
         match = re.search(r" (000\d+) \d\d au (.+).pdf", file.name)
         numero_compte = match[1]
         date_solde = parse_date(match[2])
-        texte_pea = extraire_section(
+        extract_fiscalite = extraire_section(
             contenu, "Valorisation titres (1)", "Plus/Moins value latente"
         )
 
-        logging.debug(texte_pea)
+        logging.debug(extract_fiscalite)
         valeurs_pea = [
             "Valorisation titre",
             "Solde espece",
@@ -112,14 +112,14 @@ def main():
             "Cumul versements remboursés",
             "Plus/Moins value",
         ]
-        if len(texte_pea) > 7:
+        if len(extract_fiscalite) > 7:
             midpoint = 3
             valeurs_pea = (
                 valeurs_pea[0:midpoint] + ["Désinvestissement"] + valeurs_pea[midpoint:]
             )
 
         for index, type_compte in enumerate(valeurs_pea):
-            match = re.search(r"\b(\d{1,3}(?:\s\d{3})*,\d{2})\b", texte_pea[index])
+            match = re.search(r"\b(\d{1,3}(?:\s\d{3})*,\d{2})\b", extract_fiscalite[index])
             valeur = formater_solde(match[1])
             pea.ajout_solde(
                 date_solde, numero_compte, type_compte, valeur, aligne_date=False
