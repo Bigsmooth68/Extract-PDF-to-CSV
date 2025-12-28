3080878eb7a1ab1cc76a4f85974ba56479da9955
Bigsmooth68 <olivier.spiesser@gmail.com>
2025-12-28 18:04:54 +0100

    mise en place categorisation manuel

--- classification.py
+++ classification.py
@@ -1,53 +1,63 @@
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 import pandas as pd
 import ollama
 import logging
 import sys
+import yaml
+
+class categories:
+    inverted = {}
+
+    def __init__(self, file: str = "config.yml"):
+        config = yaml.safe_load(open(file, encoding="utf-8"))
+
+        categories = config["categories"]
+
+        for item in categories:  # item = chaque dict dans la liste
+            for category, merchants in item.items():
+                for merchant in merchants:
+                    self.inverted[merchant] = category
+
+        logging.info(f"{file} chargé")
 
 
-def classify_expense(row):
-    prompt = f"""
-    Tu es un assistant qui classe les dépenses bancaires.
-    Donne un seul mot (ex: 'loyer', 'courses', 'salaire', 'loisirs', 'transport', etc.)
-    pour la catégorie de cette opération.
+    def classify_expense_manual(self, row):
+        label = row["type_compte"].strip()
 
-    date: {row["date"]}
-    compte: {row["compte"]}
-    solde: {row["solde"]}
-    type_compte: {row["type_compte"]}
-    """
-    logging.debug(f"classification {row['type_compte']}")
-    response = ollama.chat(
-        model=MODEL,
-        messages=[{"role": "user", "content": prompt}],
-    )
-    cat = response["message"]["content"].strip()
-    # on garde seulement le premier mot
-    return cat.split()[0]
+        cat = self.inverted.get(label, None)
+        if cat:
+            return cat
+        # boucle sur les motifs partiels
+        else:
+            for pattern, category in self.inverted.items():
+                if pattern in label:
+                    return category
+        # return pd.NA
 
+    @staticmethod
+    def classify_expense_auto(self, row):
+        prompt = f"""
+        Tu es un assistant qui classe les dépenses bancaires.
+        Donne un seul mot (ex: 'loyer', 'courses', 'salaire', 'loisirs', 'transport', etc.)
+        pour la catégorie de cette opération.
 
-def post_process_type_depense(row):
-    lib = row["type_compte"].upper()
-    cat = row["type_depense"]
+        date: {row["date"]}
+        compte: {row["compte"]}
+        solde: {row["solde"]}
+        type_compte: {row["type_compte"]}
+        """
+        logging.debug(f"classification {row['type_compte']}")
+        response = ollama.chat(
+            model=MODEL,
+            messages=[{"role": "user", "content": prompt}],
+        )
+        cat = response["message"]["content"].strip()
+        # on garde seulement le premier mot
+        return cat.split()[0]
 
-    # 1. Règles de surclassement par mot-clé
-    if "RETRAIT DAB" in lib:
-        return "Cash"
-    if "FREE MOBILE" in lib:
-        return "Téléphone"
-    if "CAF" in lib:
-        return "Allocations"
-    if "EKWATEUR" in lib:
-        return "Énergie"
-
-    # 2. Nettoyage de la réponse LLM
-    cat = cat.strip(" '\"").capitalize()
-
-    return cat
-
-
+#### FIN CLASS
 #### FIN FONCTIONS
 
 logging.basicConfig(
@@ -62,17 +72,39 @@ MODEL = os.environ.get("MODEL")
 engine = create_engine(url=DB_URL)
 
 try:
-    df = pd.read_sql("select * from cc", con=engine)
-    print("Connecté à la base")
+    df = pd.read_sql("select * from cc where categorie is null", con=engine)
+    logging.info("Connecté à la base")
 except Exception as X:
-    print(X)
+    logging.error(X)
+    exit(1)
 
-df = df.head(20)
+# df = df.head(100)
 
-logging.info(f"En cours d'analyse avec le model {MODEL} ...")
-df["type_depense"] = df.apply(classify_expense, axis=1)
+categories_list = categories()
 
-logging.info("Normalisation des catégories")
-df["type_depense_corrige"] = df.apply(post_process_type_depense, axis=1)
+df["categorie"] = df.apply(categories_list.classify_expense_manual, axis=1)
 
-logging.info(df)
+lignes_sans_categorie = len(df)
+
+df = df[df["categorie"].notna()]
+
+logging.info(f"Mouvements catégorisé: {len(df)} (restant {lignes_sans_categorie-len(df)})")
+
+if len(df) == 0:
+    logging.warning("Rien à catégoriser")
+    exit(1)
+
+# Ecriture des résultats
+with engine.begin() as conn:
+    for _, row in df.iterrows():
+        conn.execute(
+            text("""
+                UPDATE cc
+                SET categorie = :categorie
+                WHERE id = :id
+            """),
+            {
+                "categorie": row["categorie"],
+                "id": row["id"],
+            }
+        )
