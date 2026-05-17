e915cba05f033add4615436b28beb2203f604740
Bigsmooth68 <olivier.spiesser@gmail.com>
2026-05-17 09:58:45 +0200

    ajout csv

---
+++ compte_csv.py
@@ -0,0 +1,42 @@
+from compte import compte
+import logging
+import re
+from date_utils import parse_date
+import pandas as pd
+
+from analyse import (
+    extraire_section,
+    convertir_pdf,
+    formater_solde,
+)
+
+
+class compte_csv(compte):
+    def analyse(self_, fichier):
+        logging.debug(
+            "*************************** NEW FILE ***************************"
+        )
+        compte_solde = 0
+        logging.info(f"Analyse du fichier CSV: {fichier.name}")
+
+        numero_compte = fichier.name[:-4]
+
+        # Lecture du fichier csv
+        df = pd.read_csv(fichier, sep=",", encoding="utf-8")
+
+
+        for line in df.itertuples(index=False):
+            logging.info(f"Analyse de la ligne: {line}")
+
+            date_solde = pd.Timestamp(line.date) 
+
+            self_.ajout_solde(
+                date_solde, numero_compte, "AV", line.solde, aligne_date=False
+            )
+            compte_solde += 1
+        logging.info(f"Lignes CSV trouvées: {compte_solde}")
+
+        self_.analyse_finie(fichier)
+    
+    def __repr__(self):
+        return f"{self.lignes}"
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sqlalchemy import create_engine
 from pea import pea
 from livret import livret
 from compte_courant import compte_courant
+from compte_csv import compte_csv
 
 import locale
 
@@ -40,6 +41,11 @@ def main():
     )
     parser.add_argument(
         "-l", "--livret", action="store_true", help="Limite l'extraction aux livrets"
+    )
+    parser.add_argument(
+        "-csv",
+        action="store_true",
+        help="Limite l'extraction aux relevés des comptes csv",
     )
     parser.add_argument("-f", "--filtre", help="Filtre le nom des fichiers traités")
     parser.add_argument(
@@ -94,65 +100,75 @@ def main():
     fichiers_pea = []
     fichiers_livret = []
 
-    files = dir_path.glob("*.pdf")
+    if not args.csv:
+        pdf_files = dir_path.glob("*.pdf")
 
-    filtre = args.filtre
+        filtre = args.filtre
 
-    if filtre:
-        filtered_files = [f for f in dir_path.glob("*.pdf") if filtre in f.name]
-    else:
-        filtered_files = files
+        if filtre:
+            filtered_pdf_files = [f for f in dir_path.glob("*.pdf") if filtre in f.name]
+        else:
+            filtered_pdf_files = pdf_files
 
-    # Selection des fichiers
-    for file in sorted(filtered_files):
-        if flag_cc and "EUROCOMPTE" in file.name:
-            fichiers_cc.append(file)
-        elif flag_pea and "Portefeuille valoris" in file.name:
-            fichiers_pea.append(file)
-        elif flag_livret and (
-            "LIVRET" in file.name or "COMPTE COURANT EN CHF" in file.name
-        ):
-            fichiers_livret.append(file)
+        # Selection des fichiers
+        for file in sorted(filtered_pdf_files):
+            if flag_cc and "EUROCOMPTE" in file.name:
+                fichiers_cc.append(file)
+            elif flag_pea and "Portefeuille valoris" in file.name:
+                fichiers_pea.append(file)
+            elif flag_livret and (
+                "LIVRET" in file.name or "COMPTE COURANT EN CHF" in file.name
+            ):
+                fichiers_livret.append(file)
 
-    logging.info(
-        f"Fichiers sélectionnés: PEA {len(fichiers_pea)} - Livrets {len(fichiers_livret)} - Compte Courant {len(fichiers_cc)}"
-    )
-    if args.un:
+        logging.info(
+            f"Fichiers sélectionnés: PEA {len(fichiers_pea)} - Livrets {len(fichiers_livret)} - Compte Courant {len(fichiers_cc)}"
+        )
+        if args.un:
+            if flag_cc:
+                fichiers_cc = [fichiers_cc[0]]
+            if flag_livret:
+                fichiers_livret = [fichiers_livret[0]]
+            if flag_pea:
+                fichiers_pea = [fichiers_pea[0]]
+
         if flag_cc:
-            fichiers_cc = [fichiers_cc[0]]
-        if flag_livret:
-            fichiers_livret = [fichiers_livret[0]]
+            for fichier in fichiers_cc:
+                cc.analyse(fichier)
+
+        if not args.nf:
+            with open("config.toml", "rb") as cf:
+                config = tomllib.load(cf)
+                cc.filtrer(config["filtre"])
+
+        cc.factoriser(config["groupement"])
+
         if flag_pea:
-            fichiers_pea = [fichiers_pea[0]]
+            for fichier in fichiers_pea:
+                plan.analyse(fichier)
 
-    if flag_cc:
-        for fichier in fichiers_cc:
-            cc.analyse(fichier)
+        if flag_livret:
+            for fichier in fichiers_livret:
+                epargne.analyse(fichier)
 
-    if not args.nf:
-        with open("config.toml", "rb") as cf:
-            config = tomllib.load(cf)
-            cc.filtrer(config["filtre"])
+        logging.info(
+            f"Lignes générées: {plan.nb_lignes() + epargne.nb_lignes() + cc.nb_lignes()}"
+        )
 
-    cc.factoriser(config["groupement"])
+        epargne.fill_missing_months()
 
-    if flag_pea:
-        for fichier in fichiers_pea:
-            plan.analyse(fichier)
+        if filtre:
+            exit()
 
-    if flag_livret:
-        for fichier in fichiers_livret:
-            epargne.analyse(fichier)
+    else:
+        csv_files = dir_path.glob("*.csv")
 
-    logging.info(
-        f"Lignes générées: {plan.nb_lignes() + epargne.nb_lignes() + cc.nb_lignes()}"
-    )
-
-    epargne.fill_missing_months()
-
-    if filtre:
-        exit()
-
+        # traitement des csv
+        csv = compte_csv()
+        for file in sorted(csv_files):
+            csv.analyse(file)
+            print(csv)
+            csv.generer_insert(file.name[:-4])
     load_dotenv()
     DB_URL = os.environ.get("DB_URL")
     engine = create_engine(url=DB_URL)
